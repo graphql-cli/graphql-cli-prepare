931e2f80be40c21957f015879d3dc77c46127379
Kim Brandwijk <kim.brandwijk@gmail.com>
2017-12-29 07:20:38 +0100

    refactor: codeclimate fixes (#10)
    Fixes #4, fixes #5, fixes #6, fixes #7, fixes #8, fixes #9

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,38 @@
-import { Context, CommandObject } from 'graphql-cli' // Types only
-import { CommandModule } from 'yargs' // Types only
-import { GraphQLProjectConfig } from 'graphql-config' // Types only
+import { CommandObject, Context } from 'graphql-cli'
 
-import chalk from 'chalk'
-import { has, merge } from 'lodash'
-import * as path from 'path'
-
-import { getProjectConfig, processBundle, processBindings, saveConfig } from './prepare'
+import { Prepare } from './prepare'
 
 const command: CommandObject = {
   command: 'prepare',
   describe: 'Bundle schemas and generate bindings',
 
-  builder: argv => {
-    return argv.options({
-      output: {
-        alias: 'o',
-        describe: 'Output folder',
-        type: 'string'
-      },
-      save: {
-        alias: 's',
-        describe: 'Save settings to config file',
-        type: 'boolean',
-        default: 'false'
-      },
-      bundle: {
-        describe: 'Process schema imports',
-        type: 'boolean',
-        default: 'false'
-      },
-      bindings: {
-        describe: 'Generate bindings',
-        type: 'boolean',
-        default: 'false'
-      },
-      generator: {
-        alias: 'g',
-        describe: 'Generator used to generate bindings',
-        type: 'string'
-      }
-    })
+  builder: {
+    output: {
+      alias: 'o',
+      describe: 'Output folder',
+      type: 'string'
+    },
+    save: {
+      alias: 's',
+      describe: 'Save settings to config file',
+      type: 'boolean',
+      default: 'false'
+    },
+    bundle: {
+      describe: 'Process schema imports',
+      type: 'boolean',
+      default: 'false'
+    },
+    bindings: {
+      describe: 'Generate bindings',
+      type: 'boolean',
+      default: 'false'
+    },
+    generator: {
+      alias: 'g',
+      describe: 'Generator used to generate bindings',
+      type: 'string'
+    }
   },
 
   handler: (context: Context, argv) => {
@@ -48,54 +40,7 @@ const command: CommandObject = {
       argv.bundle = argv.bindings = true
     }
 
-    // Get projects
-    const projects: { [name: string]: GraphQLProjectConfig } = getProjectConfig(argv.project, context)
-
-    // Process each project
-    for (const projectName of Object.keys(projects)) {
-      const project = projects[projectName]
-
-      let bundleExtensionConfig: { bundle: string } | undefined
-      let bindingExtensionConfig: { binding: { output: string, generator: string } } | undefined
-
-      if (argv.bundle) {
-        if (argv.project || (!argv.project && has(project.config, 'extensions.bundle'))) {
-          context.spinner.start(`Processing schema imports for project ${chalk.green(projectName)}...`)
-          bundleExtensionConfig = processBundle(projectName, project, { output: argv.output })
-          merge(project.extensions, bundleExtensionConfig)
-          context.spinner.succeed(
-            `Bundled schema for project ${chalk.green(projectName)} written to ${chalk.green(
-              bundleExtensionConfig.bundle
-            )}`
-          )
-        } else {
-          context.spinner.info(`Bundling not configured for project ${chalk.green(projectName)}. Skipping`)
-        }
-      }
-
-      if (argv.bindings) {
-        if (argv.project || (!argv.project && has(project.config, 'extensions.binding'))) {
-          context.spinner.start(`Generating bindings for project ${chalk.green(projectName)}...`)
-          bindingExtensionConfig = processBindings(projectName, project, {
-            output: argv.output,
-            generator: argv.generator,
-            schemaPath: bundleExtensionConfig ? bundleExtensionConfig.bundle : undefined
-          })
-          merge(project.extensions, bindingExtensionConfig)
-          context.spinner.succeed(
-            `Bindings for project ${chalk.green(projectName)} written to ${chalk.green(bindingExtensionConfig.binding.output)}`
-          )
-        } else {
-          context.spinner.info(`Binding not configured for project ${chalk.green(projectName)}. Skipping`)
-        }
-      }
-
-      if (argv.save) {
-        context.spinner.start(`Saving configuration for project ${chalk.green(projectName)} to ${chalk.green(path.basename(context.getConfig().configPath))}...`)
-        saveConfig(context, project, projectName)
-        context.spinner.succeed(`Configuration for project ${chalk.green(projectName)} saved to ${chalk.green(path.basename(context.getConfig().configPath))}`)
-      }
-    }
+    new Prepare(context, argv).handle()
   }
 }
 
--- src/prepare.ts
+++ src/prepare.ts
@@ -1,3 +1,4 @@
+import chalk from 'chalk'
 import * as fs from 'fs'
 import { Context } from 'graphql-cli'
 import { GraphQLProjectConfig } from 'graphql-config'
@@ -5,170 +6,225 @@ import { importSchema } from 'graphql-import'
 import { generateCode } from 'graphql-static-binding'
 import { get, has, merge } from 'lodash'
 import * as path from 'path'
+import { Arguments } from 'yargs'
 
-export function getProjectConfig(
-  project: string,
-  context: Context
-): { [name: string]: GraphQLProjectConfig } {
-  let projects: { [name: string]: GraphQLProjectConfig } | undefined
-  if (project) {
-    if (Array.isArray(project)) {
-      projects = {}
-      project.map((p: string) => merge(projects, { [p]: context.getConfig().getProjectConfig(p) }))
-    } else {
-      // Single project mode
-      projects = { [project]: context.getProjectConfig() }
-    }
-  } else {
-    // Process all projects
-    projects = context.getConfig().getProjects()
-  }
+export class Prepare {
+  private bundleExtensionConfig: { bundle: string } | undefined
+  private projectName: string
+  private project: GraphQLProjectConfig
+  private projectDisplayName = () => chalk.green(this.projectName)
 
-  if (!projects) {
-    throw new Error('No projects defined in config file')
-  }
+  constructor(private context: Context, private argv: Arguments) {}
 
-  return projects
-}
+  handle() {
+    // Get projects
+    const projects: { [name: string]: GraphQLProjectConfig } = this.getProjectConfig()
 
-export function processBundle(
-  projectName: string,
-  project: GraphQLProjectConfig,
-  args: { output: string }
-): { bundle: string } {
-  const outputPath: string = determineOutputPath(projectName, project, args.output, 'graphql', 'bundle')
-  const schemaPath: string = determineSchemaPath(projectName, project)
+    // Process each project
+    for (const projectName of Object.keys(projects)) {
+      const project: GraphQLProjectConfig = projects[projectName]
 
-  const finalSchema = importSchema(schemaPath)
-
-  fs.writeFileSync(outputPath, finalSchema, { flag: 'w' })
-
-  return { bundle: outputPath }
-}
-
-export function processBindings(
-  projectName: string,
-  project: GraphQLProjectConfig,
-  args: { output: string; generator: string; schemaPath: string | undefined }
-): { binding: { output: string, generator: string } } {
-  const generator: string = determineGenerator(project, args.generator)
-  // TODO: This does not support custom generators
-  const extension = generator.endsWith('ts') ? 'ts' : 'js'
-  const outputPath: string = determineOutputPath(
-    projectName,
-    project,
-    args.output,
-    extension,
-    'binding.output'
-  )
-  const schema: string = determineInputSchema(args.schemaPath, project)
-
-  const schemaContents: string = fs.readFileSync(schema, 'utf-8')
-  const finalSchema: string = generateCode(schemaContents, generator)
-  fs.writeFileSync(outputPath, finalSchema, { flag: 'w' })
-
-  return { binding: { output: outputPath, generator } }
-}
-
-export function saveConfig(context, project, projectName) {
-  const config = context.getConfig()
-  config.saveConfig(project.config, projectName)
-}
-
-/**
- * Determine input schema path for binding. It uses the resulting schema from bundling (if available),
- * then looks at bundle extension (in case bundling ran before), then takes the project schemaPath.
- * Also checks if the file exists, otherwise it throws and error.
- *
- * @param {(string | undefined)} schemaPath Schema path from bundling
- * @param {GraphQLProjectConfig} project Configuration object for current project
- * @returns {string} Input schema path to be used for binding generatio.
- */
-function determineInputSchema(schemaPath: string | undefined, project: GraphQLProjectConfig): string {
-  const bundleDefined = has(project.config, 'extensions.bundle.output')
-  // schemaPath is only set when bundle ran
-  if (!schemaPath) {
-    if (bundleDefined) {
-      // Otherwise, use bundle output schema if defined
-      schemaPath = get(project.config, 'extensions.bundle.output')
-    } else if (project.schemaPath) {
-      // Otherwise, use project schemaPath
-      schemaPath = project.schemaPath
-    } else {
-      throw new Error(`Input schema cannot be determined.`)
+      this.setCurrentProject(project, projectName)
+      if (this.argv.bundle) {
+        this.bundle()
+      }
+      if (this.argv.bindings) {
+        this.bindings()
+      }
+      this.save()
     }
   }
 
-  if (fs.existsSync(schemaPath!)) {
-    return schemaPath!
-  } else {
+  setCurrentProject(project: GraphQLProjectConfig, projectName: string): void {
+    this.project = project
+    this.projectName = projectName
+  }
+
+  bindings() {
+    let bindingExtensionConfig: { binding: { output: string; generator: string } } | undefined
+
+    if (this.argv.project || (!this.argv.project && has(this.project.config, 'extensions.binding'))) {
+      this.context.spinner.start(`Generating bindings for project ${this.projectDisplayName()}...`)
+      bindingExtensionConfig = this.processBindings(
+        this.bundleExtensionConfig ? this.bundleExtensionConfig.bundle : undefined
+      )
+      merge(this.project.extensions, bindingExtensionConfig)
+      this.context.spinner.succeed(
+        `Bindings for project ${this.projectDisplayName()} written to ${chalk.green(
+          bindingExtensionConfig.binding.output
+        )}`
+      )
+    } else {
+      this.context.spinner.info(`Binding not configured for project ${this.projectDisplayName()}. Skipping`)
+    }
+  }
+
+  bundle() {
+    if (this.argv.project || (!this.argv.project && has(this.project.config, 'extensions.bundle'))) {
+      this.context.spinner.start(`Processing schema imports for project ${this.projectDisplayName()}...`)
+      this.bundleExtensionConfig = this.processBundle()
+      merge(this.project.extensions, this.bundleExtensionConfig)
+      this.context.spinner.succeed(
+        `Bundled schema for project ${this.projectDisplayName()} written to ${chalk.green(
+          this.bundleExtensionConfig.bundle
+        )}`
+      )
+    } else {
+      this.context.spinner.info(`Bundling not configured for project ${this.projectDisplayName()}. Skipping`)
+    }
+  }
+
+  save() {
+    if (this.argv.save) {
+      const configFile = path.basename(this.context.getConfig().configPath)
+      this.context.spinner.start(
+        `Saving configuration for project ${this.projectDisplayName()} to ${chalk.green(configFile)}...`
+      )
+      this.saveConfig()
+      this.context.spinner.succeed(
+        `Configuration for project ${this.projectDisplayName()} saved to ${chalk.green(configFile)}`
+      )
+    }
+  }
+
+  getProjectConfig(): { [name: string]: GraphQLProjectConfig } {
+    let projects: { [name: string]: GraphQLProjectConfig } | undefined
+    if (this.argv.project) {
+      if (Array.isArray(this.argv.project)) {
+        projects = {}
+        this.argv.project.map((p: string) =>
+          merge(projects, { [p]: this.context.getConfig().getProjectConfig(p) })
+        )
+      } else {
+        // Single project mode
+        projects = { [this.argv.project]: this.context.getProjectConfig() }
+      }
+    } else {
+      // Process all projects
+      projects = this.context.getConfig().getProjects()
+    }
+
+    if (!projects) {
+      throw new Error('No projects defined in config file')
+    }
+
+    return projects
+  }
+
+  processBundle(): { bundle: string } {
+    const outputPath: string = this.determineOutputPath('graphql', 'bundle')
+    const schemaPath: string = this.determineSchemaPath()
+
+    const finalSchema = importSchema(schemaPath)
+
+    fs.writeFileSync(outputPath, finalSchema, { flag: 'w' })
+
+    return { bundle: outputPath }
+  }
+
+  processBindings(schemaPath: string | undefined): { binding: { output: string; generator: string } } {
+    const generator: string = this.determineGenerator()
+    // TODO: This does not support custom generators
+    const extension = generator.endsWith('ts') ? 'ts' : 'js'
+    const outputPath: string = this.determineOutputPath(extension, 'binding.output')
+    const schema: string = this.determineInputSchema(schemaPath)
+
+    const schemaContents: string = fs.readFileSync(schema, 'utf-8')
+    const finalSchema: string = generateCode(schemaContents, generator)
+    fs.writeFileSync(outputPath, finalSchema, { flag: 'w' })
+
+    return { binding: { output: outputPath, generator: generator } }
+  }
+
+  saveConfig() {
+    const config = this.context.getConfig()
+    config.saveConfig(this.project.config, this.projectName)
+  }
+
+  /**
+   * Determine input schema path for binding. It uses the resulting schema from bundling (if available),
+   * then looks at bundle extension (in case bundling ran before), then takes the project schemaPath.
+   * Also checks if the file exists, otherwise it throws and error.
+   *
+   * @param {(string | undefined)} schemaPath Schema path from bundling
+   * @returns {string} Input schema path to be used for binding generatio.
+   */
+  determineInputSchema(schemaPath: string | undefined): string {
+    const bundleDefined = has(this.project.config, 'extensions.bundle.output')
+    // schemaPath is only set when bundle ran
+    if (!schemaPath) {
+      if (bundleDefined) {
+        // Otherwise, use bundle output schema if defined
+        schemaPath = get(this.project.config, 'extensions.bundle.output')
+      } else if (this.project.schemaPath) {
+        // Otherwise, use project schemaPath
+        schemaPath = this.project.schemaPath
+      } else {
+        throw new Error(`Input schema cannot be determined.`)
+      }
+    }
+
+    if (fs.existsSync(schemaPath!)) {
+      return schemaPath!
+    } else {
+      throw new Error(
+        `Schema '${schemaPath!}' not found.${bundleDefined ? ' Did you run bundle first?' : ''}`
+      )
+    }
+  }
+
+  /**
+   * Determine input schema path for bundling.
+   *
+   * @returns {string} Input schema path for bundling
+   */
+  determineSchemaPath(): string {
+    if (this.project.schemaPath) {
+      return this.project.schemaPath
+    }
+    throw new Error(`No schemaPath defined for project '${this.projectName}' in config file.`)
+  }
+
+  /**
+   * Determine generator. Provided generator takes precedence over value from config
+   *
+   * @param {string} generator Command line parameter for generator
+   * @returns {string} Generator to be used
+   */
+  determineGenerator(): string {
+    if (this.argv.generator) {
+      return this.argv.generator
+    }
+    if (has(this.project.config, 'extensions.binding.generator')) {
+      return get(this.project.config, 'extensions.binding.generator')
+    }
     throw new Error(
-      `Schema '${schemaPath!}' not found.${bundleDefined ? ' Did you run bundle first?' : ''}`
+      'Generator cannot be determined. No existing configuration found and no generator parameter specified.'
     )
   }
-}
 
-/**
- * Determine input schema path for bundling.
- *
- * @param {string} projectName Name of the current project
- * @param {GraphQLProjectConfig} project Configuration object for current project
- * @returns {string} Input schema path for bundling
- */
-function determineSchemaPath(projectName: string, project: GraphQLProjectConfig): string {
-  if (project.schemaPath) {
-    return project.schemaPath
-  }
-  throw new Error(`No schemaPath defined for project '${projectName}' in config file.`)
-}
+  /**
+   * Determine output path. Provided path takes precedence over value from config
+   *
+   * @param {string} extension File extension for output file
+   * @param {string} key Extension key containing current output setting
+   * @returns Output path
+   */
+  determineOutputPath(extension: string, key: string) {
+    let outputPath: string
+    if (this.argv.output) {
+      outputPath = path.join(this.argv.output, `${this.projectName}.${extension}`)
+    } else if (has(this.project.config, `extensions.${key}`)) {
+      outputPath = get(this.project.config, `extensions.${key}`)
+    } else {
+      throw new Error(
+        'Output path cannot be determined. No existing configuration found and no output parameter specified.'
+      )
+    }
 
-/**
- * Determine generator. Provided generator takes precedence over value from config
- *
- * @param {GraphQLProjectConfig} project Configuration object for current project
- * @param {string} generator Command line parameter for generator
- * @returns {string} Generator to be used
- */
-function determineGenerator(project: GraphQLProjectConfig, generator: string): string {
-  if (generator) {
-    return generator
+    if (!fs.existsSync(path.dirname(outputPath))) {
+      throw new Error(`Output path '${path.dirname(outputPath)}' does not exist.`)
+    }
+    return outputPath
   }
-  if (has(project.config, 'extensions.binding.generator')) {
-    return get(project.config, 'extensions.binding.generator')
-  }
-  throw new Error(
-    'Generator cannot be determined. No existing configuration found and no generator parameter specified.'
-  )
-}
-
-/**
- * Determine output path. Provided path takes precedence over value from config
- *
- * @param {GraphQLProjectConfig} project Configuration object for current project
- * @param {string} output Command line parameter for output path
- * @param {string} key Extension key containing current output setting
- * @returns Output path
- */
-function determineOutputPath(
-  projectName: string,
-  project: GraphQLProjectConfig,
-  output: string,
-  extension: string,
-  key: string
-) {
-  let outputPath: string
-  if (output) {
-    outputPath = path.join(output, `${projectName}.${extension}`)
-  } else if (has(project.config, `extensions.${key}`)) {
-    outputPath = get(project.config, `extensions.${key}`)
-  } else {
-    throw new Error(
-      'Output path cannot be determined. No existing configuration found and no output parameter specified.'
-    )
-  }
-
-  if (!fs.existsSync(path.dirname(outputPath))) {
-    throw new Error(`Output path '${path.dirname(outputPath)}' does not exist.`)
-  }
-  return outputPath
 }
